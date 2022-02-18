c4af53b1834f768814032676c9e3524952453291
vedant trivedi <vedanttrivedi83065@gmail.com>
2022-02-18 19:16:52 +0530

    done

--- app/src/main/java/com/tatvasoftassignment/assignment_7/BroadCastReceiver/BatteryStateReceiver.java
+++ app/src/main/java/com/tatvasoftassignment/assignment_7/BroadCastReceiver/BatteryStateReceiver.java
@@ -20,15 +20,17 @@ public class BatteryStateReceiver extends BroadcastReceiver {
         int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
         float batteryLevel = level * 100 / (float) scale;
         if (batteryLevel <= 5) {
-            CityForecastFragment.iconBattery.setImageResource(R.drawable.ic_battery_5_);
+            CityForecastFragment.iconBattery.setImageResource(R.drawable.ic_baseline_battery_alert_24);
         } else if (batteryLevel > 5 && batteryLevel <= 20) {
-            CityForecastFragment.iconBattery.setImageResource(R.drawable.ic_battery_20_);
-        } else if (batteryLevel > 20 && batteryLevel <= 50) {
-            CityForecastFragment.iconBattery.setImageResource(R.drawable.ic_battery_50_);
-        } else if (batteryLevel > 50 && batteryLevel <= 80) {
-            CityForecastFragment.iconBattery.setImageResource(R.drawable.ic_battery_80_);
-        } else {
-            CityForecastFragment.iconBattery.setImageResource(R.drawable.ic_battery_100_);
+            CityForecastFragment.iconBattery.setImageResource(R.drawable.ic_baseline_battery_1_bar_24);
+        } else if (batteryLevel > 20 && batteryLevel <= 40) {
+            CityForecastFragment.iconBattery.setImageResource(R.drawable.ic_baseline_battery_3_bar_24);
+        } else if (batteryLevel > 40 && batteryLevel <= 60) {
+            CityForecastFragment.iconBattery.setImageResource(R.drawable.ic_baseline_battery_5_bar_24);
+        } else if(batteryLevel > 60 && batteryLevel <= 80){
+            CityForecastFragment.iconBattery.setImageResource(R.drawable.ic_baseline_battery_6_bar_24);
+        }else{
+            CityForecastFragment.iconBattery.setImageResource(R.drawable.ic_baseline_battery_full_24);
         }
         CityForecastFragment.txtBatteryPercentage.setText(String.valueOf(batteryLevel));
     }
